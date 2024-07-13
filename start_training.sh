ca6df42a8083cc7142aa6c22427661b584c138ae
Shafiuddin Ahmed <shah7567@c3gpu-c2-u13.rc.int.colorado.edu>
2024-07-13 14:54:53 -0600

    removing cd

--- start_training.sh
+++ start_training.sh
@@ -1,30 +1,35 @@
 #! /bin/bash
 
+workspace=$1
+repo="$workspace/t5_trainer"
+py_env="$workspace/my_py"
+
+
+
 # Check if the directory /workspace/t5_trainer exists
-if [ ! -d "/workspace/t5_trainer" ]; then
+if [ ! -d "$repo" ]; then
   echo "Cloning code"
   # If the directory does not exist, clone the repository and install requirements
-  git clone https://github.com/ahmeshaf/t5_trainer.git /workspace/t5_trainer
+  git clone https://github.com/ahmeshaf/t5_trainer.git "$repo"
 fi
 
 # Check if the environment exits
-if [ ! -d "/workspace/my_py" ]; then
+if [ ! -d "$py_env" ]; then
   echo "Installing libs"
   # if not, create a new environment and install requirements
-  python3 -m venv /workspace/my_py
-  source /workspace/my_py/bin/activate
-  pip install -r /workspace/t5_trainer/requirements.txt
+  python3 -m venv "$py_env"
+  source "$py_env/bin/activate"
+  pip install -r --no-cache-dir "$repo/requirements.txt"
 fi
 
-source /workspace/my_py/bin/activate
+source "$py_env/bin/activate"
 
-cd /workspace/t5_trainer/
 d=$(date '+DATE- %m-%d-%y TIME-%H:%M:%S')
 echo "$d"
 
-output_dir=$(jq -r '.trainer.output_dir' ./config.json) && check_pt=$(ls -t "$output_dir" | head -n 1)
+output_dir=$(jq -r '.trainer.output_dir' "$repo/config.json") && check_pt=$(ls -t "$output_dir" | head -n 1)
 full_check_pt_dir="$output_dir$check_pt"
 
 echo "using checkpoint: $full_check_pt_dir"
 
-python trainer.py events-synergy/entsum_processed --config-file config.json --check-pt "$full_check_pt_dir" --kv "model_name_or_path=$full_check_pt_dir" > "continue_training.txt" &
+python trainer.py events-synergy/entsum_processed --config-file "$repo/config.json" --check-pt "$full_check_pt_dir" --kv "model_name_or_path=$full_check_pt_dir" > "$workspace/continue_training.txt" &
