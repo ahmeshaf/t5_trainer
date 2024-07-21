a7cc848cdbac17aa0a9e55c362509cbdee8ddcf0
Rehan Ahmed <ahmeshaf.rehan@gmail.com>
2024-07-21 12:55:32 -0600

    Update start_training.sh

--- start_training.sh
+++ start_training.sh
@@ -1,39 +1,20 @@
 #! /bin/bash
+export HF_HOME=/root/.cache/huggingface/
 
-workspace=$1
-repo="$workspace/t5_trainer"
-py_env="$workspace/my_py"
+apt install -y jq;
 
+CONFIG_FILE=/workspace/t5_trainer/configs/srl_ds_elaboration.json
+PYTHON_FILE=/workspace/t5_trainer/trainer.py
 
-
-# Check if the directory /workspace/t5_trainer exists
-if [ ! -d "$repo" ]; then
-  echo "Cloning code"
-  # If the directory does not exist, clone the repository and install the requirements
-  git clone https://github.com/ahmeshaf/t5_trainer.git "$repo"
-fi
-
-# Check if the environment exits
-if [ ! -d "$py_env" ]; then
-  echo "Installing libs"
-  # if not, create a new environment and install requirements
-  python3 -m venv "$py_env"
-  source "$py_env/bin/activate"
-  pip install --cache-dir "$1/.pip/" -r "$repo/requirements.txt"
-fi
-
-source "$py_env/bin/activate"
-
-d=$(date '+DATE- %m-%d-%y TIME-%H:%M:%S')
-echo "$d"
+source /workspace/venv/bin/activate
 
 # identify the latest checkpoint
-output_dir=$(jq -r '.trainer.output_dir' "$repo/config.json") && check_pt=$(ls -t "$output_dir" | head -n 1)
-full_check_pt_dir="$output_dir$check_pt"
+OUTPUT_DIR=$(jq -r '.trainer.output_dir' "$CONFIG_FILE") && check_pt=$(ls -t "$OUTPUT_DIR" | head -n 1)
+full_check_pt_dir="$OUTPUT_DIR$check_pt"
 
 # If no checkpoint, start from scratch, else continue from chkpt
 if [ -z "$check_pt" ]; then 
-  echo "No checkpoint. Running from scratch!" && python "$repo/trainer.py" events-synergy/entsum_processed --config-file "$repo/config.json" 
+  echo "No checkpoint. Running from scratch!" && python "$PYTHON_FILE" "$CONFIG_FILE"
 else 
-  echo "Using checkpoint: $full_check_pt_dir" && python "$repo/trainer.py" events-synergy/entsum_processed --config-file "$repo/config.json" --check-pt "$full_check_pt_dir" --kv "model_name_or_path=$full_check_pt_dir"
+  echo "Using checkpoint: $full_check_pt_dir" && python "$PYTHON_FILE" "$CONFIG_FILE" --check-pt "$full_check_pt_dir"
 fi
